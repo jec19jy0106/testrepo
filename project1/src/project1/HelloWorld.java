892afd07c8cd4080a89124980c39f499ac090494
19jy0106 <19jy0106@PC22.jynet2.jec.ac.jp>
2020-10-26 15:04:07 +0900

    HTML5を表示する行を新たに追加

--- project1/src/project1/HelloWorld.java
+++ project1/src/project1/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("HelloWorld");
-		System.out.println("java");
+		System.out.println("Java");
+		System.out.println("HTML5");
 	}
 
 }
